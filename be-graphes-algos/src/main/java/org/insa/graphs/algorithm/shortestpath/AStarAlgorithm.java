24c5d32ae98a14f961af25b379ea2aff0686ba5e
Celia <chauzy.celia@gmail.com>
2021-05-20 18:01:41 +0200

    update A* PCC temps

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -1,5 +1,6 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.algorithm.AbstractInputData.Mode;
 import org.insa.graphs.model.*;
 //import org.insa.graphs.model.Label;
 
@@ -10,11 +11,22 @@ public class AStarAlgorithm extends DijkstraAlgorithm {
     }
 
     protected void Initialisation(Label[] Labels, Graph graph, ShortestPathData data) {
-    	for (int i = 0 ; i < graph.size(); i++) {
-    		//distance à vol d'oiseau = distance entre deux points
-    		double distance = Point.distance(graph.get(i).getPoint(), data.getDestination().getPoint());
-        	Labels[i] = new LabelStar(i, false, Double.POSITIVE_INFINITY, null, distance) ;
-        }
+    	//cas du plus court chemin en temps
+    	if (data.getMode() == Mode.TIME) {
+    		double vitesse = (double) data.getGraph().getGraphInformation().getMaximumSpeed()/3.6 ; //car on a des km/h et la distance est en m
+    		for (int i = 0 ; i < graph.size(); i++) {
+        		//distance à vol d'oiseau = distance entre deux points
+        		double distance = Point.distance(graph.get(i).getPoint(), data.getDestination().getPoint());
+            	Labels[i] = new LabelStar(i, false, Double.POSITIVE_INFINITY, null, distance/vitesse) ;
+            }     
+    	}
+    	//cas du plus court chemin en distance
+    	else {
+	    	for (int i = 0 ; i < graph.size(); i++) {
+	    		//distance à vol d'oiseau = distance entre deux points
+	    		double distance = Point.distance(graph.get(i).getPoint(), data.getDestination().getPoint());
+	        	Labels[i] = new LabelStar(i, false, Double.POSITIVE_INFINITY, null, distance) ;
+	        } 
+    	}
    }
-
 }
